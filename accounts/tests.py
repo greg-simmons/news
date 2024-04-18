b344e4f7c91d6e69092d06b8dd0e7950cbe81d8b
gregtsimmons <gregtsimmons@gmail.com>
2024-04-18 16:35:08 +0900

    add Bootstrap styling

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse  # new
+from django.urls import reverse
 
 class UsersManagersTests(TestCase):
     ...
 
-class SignupPageTests(TestCase):  # new
+class SignupPageTests(TestCase):
     def test_url_exists_at_correct_location_signupview(self):
         response = self.client.get("/accounts/signup/")
         self.assertEqual(response.status_code, 200)
